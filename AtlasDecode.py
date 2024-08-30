6a4956890dfd1c07b55aaaaf4859574d62b55d54
Yifang-Yuan <yifangyyf@gmail.com>
2024-08-30 22:23:51 +0100

    Atals_decode

---
+++ AtlasDecode.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 15:18:25 2024
+
+@author: Yifang
+"""
+import os
+from scipy.io import loadmat
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from SPADPhotometryAnalysis import SPADAnalysisTools as Analysis
+from SPADPhotometryAnalysis import photometry_functions as fp
+from scipy.ndimage import uniform_filter1d
+
+def loadPCFrame (readData):
+    '''loads a sensor bytestream file containing a single Photon Count frame
+        into a bytearray that can be used to perform operations on in python
+    '''
+    orig = np.repeat((np.repeat(np.arange(8).reshape((1, 8)), 128, 0)), 16, 0).reshape((128, 128))
+    add = np.repeat(np.array(np.repeat(np.arange(16).reshape(1, 16), 128, 0)), 8, 1) * 1024
+    add2 = np.repeat(np.array(np.arange(128).reshape(128, 1)), 128, 1) * 8
+    lut = orig + add + add2
+    insertionIndices = np.repeat(np.array(np.arange(1024)), 20, 0)*44
+
+    readData = np.insert(readData, insertionIndices, 0, 0)    #insert pads to make stream 32 bit ints BEFORE unpacking and reordering
+    #print("padded array shape: {}".format(readData.shape))      #16384 pixels * 4 bytes = 65536 bytes
+    readData = readData.reshape((-1, 2))            #reshape to match output of ATLAS
+    readData = np.roll(readData, 1, 1)              #get top and bottom half of image correct (swap top and bottom half of image)
+    readData = np.unpackbits(readData, 1)
+    readData = np.split(readData, 1024, 0)
+    readData = np.packbits(readData, 1, 'big')
+    readData = np.flip(readData, 1)
+    readData = np.swapaxes(readData, 1, 2)
+    readData = np.ascontiguousarray(readData)
+    readData = readData.view(np.int32)
+    readData = np.swapaxes(readData, 0, 1)
+    readData = readData.reshape((-1, 1))            #reshape to match output of ATLAS
+    readData = readData[lut]
+    return readData
+    
+def remove_hotpixel(readData,hotpixel_photon_thre=2000):
+    readData[:,:,0][readData[:,:,0] > hotpixel_photon_thre] = 0
+    return readData
+
+def find_hotpixel_idx(hotpixel_path,array_data,hotpixel_photon_thre=2000):
+    index_array = np.argwhere(array_data > hotpixel_photon_thre)
+    np.savetxt(hotpixel_path, index_array, fmt='%d', delimiter=',')
+    return index_array
+
+def decode_atlas_folder (folderpath,hotpixel_path,hotpixel_photon_thre=2000):
+    files = os.listdir(folderpath)
+    # Filter out the CSV files that match the pattern 'AnimalTracking_*.csv'
+    frame_files = [file for file in files if file.startswith('frame_') and file.endswith('.mat')]
+    # Sort the CSV files based on the numerical digits in their filenames
+    sorted_mat_files = sorted(frame_files, key=lambda x: int(x.split('_')[-1].split('.')[0]))
+    #print (sorted_mat_files)
+    pixel_arrays = []
+    hotpixel_indices= np.loadtxt(hotpixel_path, delimiter=',', dtype=int)
+    i=0
+    for file in sorted_mat_files:
+        #print ('framefile name', file)
+        single_frame_data_path = os.path.join(folderpath, file)
+        matdata = loadmat(single_frame_data_path)
+        real_data=matdata['realData']
+        readData=loadPCFrame(real_data) #decode data to single pixel frame
+        readData=remove_hotpixel(readData,hotpixel_photon_thre) #REMOVE hotpixel by a threshold
+        single_pixel_array=readData[:,:,0]
+        single_pixel_array[hotpixel_indices[:, 0], hotpixel_indices[:, 1]] = 0 #REMOVE HOTPIXEL FROM MASK
+        i=i+1
+        if i>0:
+            pixel_arrays.append(single_pixel_array)
+    pixel_array_all_frames = np.stack(pixel_arrays, axis=2)
+    sum_pixel_array = np.sum(pixel_array_all_frames, axis=2)
+    avg_pixel_array =np.mean(pixel_array_all_frames, axis=2)
+    
+    return pixel_array_all_frames,sum_pixel_array,avg_pixel_array
+
+def show_image_with_pixel_array(pixel_array_2d,showPixel_label=True):
+    vmin = 0  # Minimum value
+    vmax = 80  # Maximum value
+
+    plt.imshow(pixel_array_2d, cmap='gray', vmin=vmin, vmax=vmax)
+    #plt.imshow(pixel_array_2d, cmap='gray')
+    # Add color bar with photon count numbers
+    if showPixel_label:
+        # Add x and y axes with pixel IDs
+        plt.xticks(np.arange(0, 128, 10), labels=np.arange(0, 128, 10))
+        plt.yticks(np.arange(0, 128, 10), labels=np.arange(0, 128, 10))
+        cbar = plt.colorbar()
+        cbar.set_label('Photon Count')
+    else:
+        plt.axis('off')  # Turn off axis
+    plt.show()
+    return -1
+
+def pixel_array_plot_hist(pixel_array, plot_min_thre=100):
+    photon_counts = pixel_array.flatten()
+    # Plot the histogram
+    plt.hist(photon_counts, bins=50, range=(plot_min_thre, photon_counts.max()), color='blue', alpha=0.7)
+    #plt.hist(photon_counts, bins=50, range=(0, 500), color='blue', alpha=0.7)
+    plt.xlabel('Photon Count')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Photon Counts')
+    plt.grid(True)
+    plt.show()
+    return -1
+
+def get_trace_from_3d_pixel_array(pixel_array_all_frames,pixel_array,xxrange,yyrange):
+    import matplotlib.patches as patches
+    plt.figure(figsize=(6, 6))
+    plt.imshow(pixel_array, cmap='gray')
+    plt.colorbar(label='Photon count')
+    plt.title('Image with Selected Region')
+    plt.xlabel('X coordinate')
+    plt.ylabel('Y coordinate')
+    # Add a rectangle around the selected region
+    rect = patches.Rectangle((xxrange[0], yyrange[0]), xxrange[1]-xxrange[0], yyrange[1]-yyrange[0], 
+                             linewidth=2, edgecolor='r', facecolor='none')
+    plt.gca().add_patch(rect)
+    plt.show()
+    region_pixel_array = pixel_array_all_frames[xxrange[0]:xxrange[1]+1, yyrange[0]:yyrange[1]+1, :]
+    mean_array=np.mean(region_pixel_array, axis=2)
+    std_array=np.std(region_pixel_array, axis=2)
+    #Check hotpixels
+    # Sum along the x and y axes to get the sum of photon counts within the specified range for each frame
+    sum_values_over_time = np.sum(region_pixel_array, axis=(0, 1))
+    mean_values_over_time = np.mean(region_pixel_array, axis=(0, 1))
+    return sum_values_over_time,mean_values_over_time,region_pixel_array
+
+def plot_trace(trace,ax, fs=1017, label="trace"):
+    t=(len(trace)) / fs
+    taxis = np.arange(len(trace)) / fs
+    mean_trace = np.mean(trace)
+    #ax.plot(taxis,trace,linewidth=1,label=label)
+    ax.plot(taxis,trace,linewidth=1)
+    #ax.axhline(mean_trace, color='r', linestyle='--', label='Mean Value', linewidth=1.5)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    # ax.spines['left'].set_visible(False)
+    # ax.spines['bottom'].set_visible(False)
+    #ax.xaxis.set_visible(False)  # Hide x-axis
+    #ax.yaxis.set_visible(False)  # Hide x-axis
+    ax.set_xlim(0,t)
+    ax.legend(loc="upper right", frameon=False)
+    ax.set_xlabel('Time(second)')
+    ax.set_ylabel('Photon Count')
+    return ax
+
+def replace_outliers_with_nearest_avg(data, window_size=25000, z_thresh=3):
+    # Calculate the mean and standard deviation of the moving window
+    mean = uniform_filter1d(data, window_size, mode='reflect')
+    std = uniform_filter1d(data**2, window_size, mode='reflect')
+    std = np.sqrt(std - mean**2)
+
+    # Identify the outliers
+    outliers = (np.abs(data - mean) > z_thresh * std)
+
+    # Replace outliers with the average of their nearest non-outlier neighbors
+    for i in np.where(outliers)[0]:
+        j = i - 1
+        while j >= 0 and outliers[j]:
+            j -= 1
+        k = i + 1
+        while k < len(data) and outliers[k]:
+            k += 1
+        if j >= 0 and k < len(data):
+            data[i] = (data[j] + data[k]) / 2
+        elif j >= 0:
+            data[i] = data[j]
+        elif k < len(data):
+            data[i] = data[k]
+
+    return data
+
+def get_zscore_from_atlas_continuous (dpath,hotpixel_path,xxrange= [25, 85],yyrange= [30, 90],fs=840,hotpixel_photon_thre=2000):
+    pixel_array_all_frames,sum_pixel_array,_=decode_atlas_folder (dpath,hotpixel_path,hotpixel_photon_thre=hotpixel_photon_thre)
+    _,mean_values_over_time,_=get_trace_from_3d_pixel_array(pixel_array_all_frames,sum_pixel_array,xxrange,yyrange)
+    #print('original lenth: ', len(mean_values_over_time))
+    Trace_raw=mean_values_over_time[1:]
+    #print('trace_raw lenth 1: ', len(Trace_raw))
+    Trace_raw = np.append(Trace_raw, Trace_raw[-1])
+    #print('trace_raw lenth 2: ', len(Trace_raw))
+    fig, ax = plt.subplots(figsize=(8, 2))
+    plot_trace(Trace_raw,ax, fs, label="raw_data")
+    
+    #remove outlier
+    #Trace_raw=replace_outliers_with_nearest_avg(Trace_raw, window_size=25000, z_thresh=4)
+    
+    lambd = 10e3 # Adjust lambda to get the best fit
+    porder = 1
+    itermax = 15
+    sig_base=fp.airPLS(Trace_raw,lambda_=lambd,porder=porder,itermax=itermax) 
+    signal = (Trace_raw - sig_base)  
+    z_score=(signal - np.median(signal)) / np.std(signal)
+    
+    fig, ax = plt.subplots(figsize=(8, 2))
+    plot_trace(z_score,ax, fs, label="zscore")
+    return Trace_raw,z_score
+
---
+++ BatchPreReadAtlasFolder.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 21:53:34 2024
+
+@author: Yifang
+"""
+
+import AtlasDecode
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+def replace_outliers_with_avg(data, threshold):
+    # Identify the outliers
+    outliers = np.abs(data) > threshold
+
+    # Get the indices of the outliers
+    outlier_indices = np.where(outliers)[0]
+
+    # Iterate over the outlier indices
+    for index in outlier_indices:
+        # Find the nearest non-outlier value
+        left = index
+        while left > 0 and outliers[left - 1]:
+            left -= 1
+
+        right = index
+        while right < len(data) - 1 and outliers[right + 1]:
+            right += 1
+
+        # If the outlier is not at the boundaries of the array, replace it with the average of its neighbors
+        if left != 0 and right != len(data) - 1:
+            data[index] = (data[left - 1] + data[right + 1]) / 2
+        # If the outlier is at the boundaries of the array, replace it with the nearest non-outlier value
+        else:
+            data[index] = data[left - 1] if left != 0 else data[right + 1]
+
+    return data
+
+def plot_trace(trace,ax, fs=9938.4, label="trace",color='tab:blue'):
+    t=(len(trace)) / fs
+    taxis = np.arange(len(trace)) / fs
+    ax.plot(taxis,trace,linewidth=0.5,label=label,color=color)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.set_xlim(0,t)
+    ax.legend(loc="upper right", frameon=False)
+    #ax.spines['left'].set_visible(False)
+    #ax.spines['bottom'].set_visible(False)
+    #ax.xaxis.set_visible(False)  # Hide x-axis
+    #ax.yaxis.set_visible(False)  # Hide x-axis
+    ax.set_xlabel('Time(second)')
+    ax.set_ylabel('Photon Count')
+    return ax
+
+def Remove_outliers_for_session (parent_folder,TargetfolderName='SyncRecording'):
+    
+    # List all files and directories in the parent folder
+    all_contents = os.listdir(parent_folder)
+    # Filter out directories containing the target string
+    sync_recording_folders = [folder for folder in all_contents if TargetfolderName in folder]
+    # Define a custom sorting key function to sort folders in numeric order
+    def numeric_sort_key(folder_name):
+        return int(folder_name.lstrip(TargetfolderName))
+    # Sort the folders in numeric order
+    sync_recording_folders.sort(key=numeric_sort_key)
+    # Iterate over each sync recording folder
+    for SyncRecordingName in sync_recording_folders:
+        # Now you can perform operations on each folder, such as reading files inside it
+        print("----Now processing folder:", SyncRecordingName)
+        filename=os.path.join(parent_folder, SyncRecordingName,'Zscore_traceAll.csv')
+        trace = np.genfromtxt(filename, delimiter=',')
+        np.savetxt(filename, trace, delimiter=',', comments='')
+        fig, ax = plt.subplots(figsize=(8,2))
+        plot_trace(trace,ax, fs=840,label='z_score')
+        #trace=replace_outliers_with_nearest_avg(trace, window_size=25000, z_thresh=4)
+        trace=replace_outliers_with_avg(trace, threshold=4)
+        fig, ax = plt.subplots(figsize=(8,2))
+        plot_trace(trace,ax, fs=840,label='cleaned_z_score')
+        np.savetxt(filename, trace, delimiter=',', comments='')
+
+    return -1
+def extract_timestamp(folder_name):
+    # Assuming the timestamp is at the end in the format "YYYY-MM-DD_HH-MM"
+    timestamp_str = folder_name.split('_')[-2]+ '_' +folder_name.split('_')[-1] 
+    print ('timestamp_str----',timestamp_str)
+    # Convert the string to a datetime object
+    return datetime.strptime(timestamp_str, '%Y-%m-%d_%H-%M')
+
+def read_multiple_Atlas_bin_folder(mouse_folder,hotpixel_path,xxRange,yyRange,new_folder_name='SyncRecording',hotpixel_photon_thre=500):
+    '''When using this batch processing function, please make sure the ROI did not change for this whole experiment.'''
+    # Get a list of all directories in the parent folder
+    day_directories = [os.path.join(mouse_folder, d) for d in os.listdir(mouse_folder) if os.path.isdir(os.path.join(mouse_folder, d))]
+    # Sort the directories by creation time
+    day_directories = sorted(day_directories)
+    # Read the directories in sorted order
+    for day_folder in day_directories:
+        print ('day_folder--', day_folder)
+        all_atlas_folders = os.listdir(day_folder)
+        # Filter out only the folders (directories)
+        folders = [item for item in all_atlas_folders if os.path.isdir(os.path.join(day_folder, item))]
+        # Sort folders by the extracted timestamp
+        sorted_atlas_folders = sorted(folders, key=extract_timestamp)
+        i=0
+        for directory in sorted_atlas_folders:
+            print("current_atlas_Folder:", directory)
+            Trace_raw,z_score=AtlasDecode.get_zscore_from_atlas_continuous (directory,hotpixel_path,
+                                                                            xxrange= xxRange,yyrange= yyRange,fs=840,hotpixel_photon_thre=hotpixel_photon_thre)
+            
+            i=i+1
+            folder_name = f'{new_folder_name}{i}'
+            save_folder = os.path.join(day_folder, folder_name)
+            print ('save_folder is', save_folder)
+            
+            #Remove outliers
+            #z_score=replace_outliers_with_avg(z_score, threshold=4)
+            
+            # Create the folder if it doesn't exist
+            if not os.path.exists(save_folder):
+                os.makedirs(save_folder)
+            # np.savetxt(os.path.join(save_folder,'Zscore_traceAll.csv'), z_score, delimiter=',', comments='')
+            # np.savetxt(os.path.join(save_folder,'Green_traceAll.csv'), Trace_raw, delimiter=',', comments='')
+            # np.savetxt(os.path.join(save_folder,'Red_traceAll.csv'), Trace_raw, delimiter=',', comments='')
+    return -1
+
+
+def main():
+    '''Set the folder to the one-day recording folder that contain SPAD data for each trial'''
+    '''IMPORTANT: Set the ROI range, this can be found by the screenshot you made during recording,
+    or draw image for a single trial using DemoS ingleSPAD_folder.py'''
+    
+    'Reading SPAD binary data'
+    hotpixel_path='E:/YYFstudy/OptoEphysAnalysis/Altas_hotpixel.csv'
+    xxrange = [45, 85]
+    yyrange = [50, 90]
+    # xxrange = [5, 55]
+    # yyrange = [25, 75]
+
+
+    mouse_folder='G:/Mingshuai_atlas/1769568_PV_mNeon/'
+    read_multiple_Atlas_bin_folder(mouse_folder,hotpixel_path,xxrange,yyrange,new_folder_name='Atlas_Trial')
+    
+    
+if __name__ == "__main__":
+    main()
