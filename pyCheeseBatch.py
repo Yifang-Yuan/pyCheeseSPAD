ac21c7bec56209d1b28d1c3dab7083c0ff5645ff
PIG-ZMS <pig.mingshuaizhu@gmail.com>
2024-09-04 12:25:54 +0100

    Update pyCheeseBatch.py

--- pyCheeseBatch.py
+++ pyCheeseBatch.py
@@ -40,15 +40,19 @@ def Read_MultiDays_Save_CB_SB_results (total_days,parent_folder,save_folder,COLD
 
 #%%
 'This is to call the above function to read all sessions in multiple days for an animal'
-grandparent_folder = 'D:/CheeseboardYY/GCaMP8m/'
+grandparent_folder = 'E:/Mingshuai/workingfolder/Group B1/'
 output_folder = grandparent_folder+'output/'
-parent_list = ['1769565','1804115']
+parent_list = ['1681628','1769565','1804115']
 before_window=5
 after_window=5
 PlotSB = True
 for i in range (len (parent_list)):
     parent_folder = grandparent_folder+parent_list[i]+'/'
-    COLD_folder = parent_folder+'Cold_folder/'
+    
+    for filename in os.listdir(parent_folder):
+        if 'Cold_folder' in filename or 'COLD_folder' in filename:
+            COLD_folder = os.path.join(parent_folder,filename)+'/'
+    
     save_folder = parent_folder
     result_folder = parent_folder+'results/'
     if not os.path.exists(result_folder):
@@ -70,15 +74,15 @@ for i in range (len (parent_list)):
     if not SB:
         PlotSB = False
     print('Now reading in:'+parent_list[i])
-    #Read_MultiDays_Save_CB_SB_results (total_days,parent_folder,save_folder,COLD_folder,animalID,before_window,after_window,SB=SB)
+    Read_MultiDays_Save_CB_SB_results (total_days,parent_folder,save_folder,COLD_folder,animalID,before_window,after_window,SB=SB)
 
     '''plot well1 and well2 average PETH for all sessions'''
     ''' you need to put all the PETH files with the same half window in the same folder '''
-    #plotCheese.plot_2wells_PETH_all_trials (result_folder,2,2,animalID)
-    #plotCheese.plot_day_average_PETH_together(result_folder,2,2,animalID)
+    plotCheese.plot_2wells_PETH_all_trials (result_folder,2,2,animalID)
+    plotCheese.plot_day_average_PETH_together(result_folder,2,2,animalID)
     plotCheese.plot_SB_PETH_all_trials (result_folder,3,5,animalID)
     plotCheese.plot_day_average_SB_PETH_together (result_folder,3,5,animalID)
         
-    #Reward_Latency.PlotRouteScoreGraph(COLD_folder,result_folder,output_folder)
+    Reward_Latency.PlotRouteScoreGraph(COLD_folder,result_folder,output_folder)
 
-#MultipleRouteScore.PlotRSForMultipleMouse(output_folder,output_folder,'route_score', 'z_dif')
+MultipleRouteScore.PlotRSForMultipleMouse(output_folder,output_folder,'route_score', 'z_dif')
